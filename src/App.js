7ba7aa34ca15d5ee0e292f57cd48f3dddd89d2f6
Jacob Spade <spade.jacob@Yahoo.com>
2019-04-22 19:40:48 -0700

    created navbar

--- src/App.js
+++ src/App.js
@@ -4,7 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Jacob Spade
+      <nav>
+        <h2 className="logo"><a class="logo-link" href="#">Oscars 2019</a></h2>
+        <ul className="nav-menu">
+          <li><a className="nav-menu__link" href="#">Home</a></li>
+          <li><a className="nav-menu__link" href="#">Best Actor</a></li>
+          <li><a className="nav-menu__link" href="#">Best Actress</a></li>
+          <li><a className="nav-menu__link" href="#">Best Films</a></li>
+        </ul>
+      </nav>
     </div>
   );
 }
